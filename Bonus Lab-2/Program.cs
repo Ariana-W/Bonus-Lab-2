a0f2f0370b30d6a2da98283b6e5e8ccc753370cd
Ariana-W <ariana.waller@yahoo.com>
2017-08-03 16:07:51 -0400

    Initial Commit

---
+++ Bonus Lab-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in a number ");
+            Console.WriteLine();
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("You just entered: " + userInput);
+
+            int finalAnswer = userInput + 1;
+
+            Console.WriteLine("Final answer: " + finalAnswer);
+
+
+        }
+    }
+}
